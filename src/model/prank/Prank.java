79a0468154071a4dd0a02bcfab9f704fe1e92d22
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-04-18 13:21:50 +0200

    Updated Prank.java

---
+++ src/model/prank/Prank.java
@@ -0,0 +1,54 @@
+package model.prank;
+/*
+ * Projet : Labo-SMTP
+ * Créé le 18.04.2016.
+ * Auteur : Adriano Ruberto
+ */
+
+import model.mail.Message;
+import model.mail.Person;
+
+import java.util.LinkedList;
+
+public class Prank {
+
+   private final LinkedList<Person> victimR = new LinkedList<>();
+   private final LinkedList<Person> witnessR = new LinkedList<>();
+   private Person sender;
+   private String message;
+
+   public LinkedList<Person> getVictimRecipients() {
+	  return victimR;
+   }
+
+   public LinkedList<Person> getWitnessRecipients() {
+	  return witnessR;
+   }
+
+   public void addVictimRecipients(LinkedList<Person> ps) {
+	  victimR.addAll(ps);
+   }
+
+   public void addWitnessRecipients(LinkedList<Person> ps) {
+	  witnessR.addAll(ps);
+   }
+
+   public Person getVictimSender() {
+	  return sender;
+   }
+
+   public String getMessage() {
+	  return message;
+   }
+
+   public void setMessage(String message) {
+	  this.message = message;
+   }
+
+   public Message generateMessage() {
+	  Message msg = new Message();
+	  msg.setBody(this.message + "\r\n" + sender.getFirstName());
+
+   }
+
+}
